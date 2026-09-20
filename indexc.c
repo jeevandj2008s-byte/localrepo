7958413726199fdbd2b052e28a3afddfc75b70fd
jeevandj2000s-byte <jeevandj2008s@gmail.com>
2026-09-21 01:21:24 +0530

    new feature2

--- indexc.c
+++ indexc.c
@@ -3,5 +3,7 @@ int main(){
     printf("my name is jeevan \n");
     printf("this is a prial reporostory");
     printf("this is a new feature");
-    return;
+    printf("thid is also a new feature");
+    printf("i was checking what all i can do with switching branches");
+    return 0;
 }
